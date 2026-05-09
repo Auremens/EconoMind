5b5264a6b0b1050e17de46062494b93b23bd03ad
Auremens <auremen91@gmail.com>
2026-05-09 18:03:39 +0200

    Add files via upload

--- economind/src/pages/_document.tsx
+++ economind/src/pages/_document.tsx
@@ -3,7 +3,30 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="fr">
-      <Head />
+      <Head>
+        {/* Apply dark mode before paint to avoid flash */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function() {
+                try {
+                  var data = localStorage.getItem('economind_v1');
+                  var parsed = data ? JSON.parse(data) : null;
+                  // Default is dark mode
+                  var isDark = parsed ? parsed.darkMode !== false : true;
+                  if (isDark) {
+                    document.documentElement.classList.add('dark');
+                  } else {
+                    document.documentElement.classList.remove('dark');
+                  }
+                } catch(e) {
+                  document.documentElement.classList.add('dark');
+                }
+              })();
+            `,
+          }}
+        />
+      </Head>
       <body>
         <Main />
         <NextScript />
